7849697a34426bcbf5b237b5375f8397f5d71214
Najm Ajmal <najmajmal@mail.com>
2023-08-08 04:38:27 -0700

    Update gh-redirects.js

--- gh-redirects.js
+++ gh-redirects.js
@@ -4,7 +4,7 @@ const ghQueryParam = queryParams.get("gh");
 // Redirect to GitHub with referral after 5 seconds
 if (ghQueryParam) {
   const referralValue = "https://github.com/SintcoLTD/url";
-  const githubProfileUrl = `https://github.com/${ghQueryParam}?refer=${encodeURIComponent(referralValue)}`;
+  const githubProfileUrl = `https://github.com/${ghQueryParam}?refer=${referralValue}`;
   setTimeout(() => {
     window.location.href = githubProfileUrl;
   }, 5000); // 5000 milliseconds = 5 seconds
