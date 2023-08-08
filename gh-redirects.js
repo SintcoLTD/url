a1f5511ff867a4186cdfd4319719503496fdde3a
Najm Ajmal <najmajmal@mail.com>
2023-08-08 03:11:41 -0700

    Create gh-redirects.js

---
+++ gh-redirects.js
@@ -0,0 +1,13 @@
+// Get the query parameter from the URL
+const queryParams = new URLSearchParams(window.location.search);
+const githubUsername = queryParams.get("username");
+
+if (githubUsername) {
+  // Delay the redirect by 5 seconds
+  setTimeout(() => {
+    const githubProfileUrl = `https://github.com/${githubUsername}?refer=sintco-url-refer`;
+    window.location.href = githubProfileUrl;
+  }, 5000); // 5000 milliseconds = 5 seconds
+} else {
+  console.error("GitHub username not provided in query parameter.");
+}
