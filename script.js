b362e9a0514609ce5e6c310e1c17f3b403e9027d
Najm Ajmal <najmajmal@mail.com>
2023-08-08 03:23:01 -0700

    Update script.js

--- script.js
+++ script.js
@@ -7,15 +7,27 @@ function handleSearch(event) {
   }
 }
 
-// Function to redirect to the provided URL with the refer parameter
-function redirectToUrl(url) {
-  const referralValue = "https://github.com/SintcoLTD/url";
-  const finalUrl = `${url}?refer=${referralValue}`;
-  
-  // Redirect the user after a short delay (2 seconds)
-  setTimeout(() => {
-    window.location.href = finalUrl;
-  }, 2000); // 2000 milliseconds = 2 seconds
+// Function to redirect to the provided URL from url.json
+function redirectToUrl(query) {
+  fetch("url.json")
+    .then(response => response.json())
+    .then(urlMappings => {
+      const targetUrl = urlMappings[query];
+      if (targetUrl) {
+        // Add the refer parameter and redirect after a short delay (2 seconds)
+        const referralValue = "https://github.com/SintcoLTD/url";
+        const finalUrl = `${targetUrl}?refer=${referralValue}`;
+        setTimeout(() => {
+          window.location.href = finalUrl;
+        }, 2000); // 2000 milliseconds = 2 seconds
+      } else {
+        console.error("URL not found in url.json");
+      }
+    })
+    .catch(error => {
+      console.error("Error fetching url.json:", error);
+      alert("An error occurred while fetching the URL mappings.");
+    });
 }
 
 // Listen for form submission
