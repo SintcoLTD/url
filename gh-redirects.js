3c517884764412d957dcf47889c49071f49a1550
Najm Ajmal <najmajmal@mail.com>
2023-08-08 04:34:28 -0700

    Update gh-redirects.js

--- gh-redirects.js
+++ gh-redirects.js
@@ -1,35 +1,13 @@
-const countdownElement = document.getElementById("countdown");
-const visitNowBtn = document.getElementById("visitNowBtn");
+const queryParams = new URLSearchParams(window.location.search);
+const ghQueryParam = queryParams.get("gh");
 
-// Set the initial countdown time in seconds
-let countdownTime = 5;
-
-// Function to update the countdown and check for redirection
-function updateCountdown() {
-  countdownElement.textContent = countdownTime;
-  if (countdownTime <= 0) {
-    redirectToGitHub();
-  } else {
-    countdownTime--;
-    setTimeout(updateCountdown, 1000); // Update every 1 second
-  }
-}
-
-// Redirect to GitHub with referral
-function redirectToGitHub() {
-  const queryParams = new URLSearchParams(window.location.search);
-  const ghQueryParam = queryParams.get("gh");
-  if (ghQueryParam) {
-    const referralValue = "https://github.com/SintcoLTD/url";
-    const githubProfileUrl = `https://github.com/${ghQueryParam}?refer=${encodeURIComponent(referralValue)}`;
+// Redirect to GitHub with referral after 5 seconds
+if (ghQueryParam) {
+  const referralValue = "https://github.com/SintcoLTD/url";
+  const githubProfileUrl = `https://github.com/${ghQueryParam}?refer=${encodeURIComponent(referralValue)}`;
+  setTimeout(() => {
     window.location.href = githubProfileUrl;
-  } else {
-    console.error("GitHub username not provided in query parameter.");
-  }
+  }, 5000); // 5000 milliseconds = 5 seconds
+} else {
+  console.error("GitHub username not provided in query parameter.");
 }
-
-// Listen for "Visit Now" button click
-visitNowBtn.addEventListener("click", redirectToGitHub);
-
-// Start the countdown on page load
-updateCountdown();
