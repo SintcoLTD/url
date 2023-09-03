4558bb71e71297c55469c8815444e4c55fe1680b
Najm Ajmal <130285427+NajmAjmal@users.noreply.github.com>
2023-09-03 07:51:41 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -7,27 +7,20 @@ function handleSearch(event) {
   }
 }
 
-// Function to redirect to the provided URL from url.json
+// Function to redirect to the provided URL from the url parameter
 function redirectToUrl(query) {
-  fetch("url.json")
-    .then(response => response.json())
-    .then(urlMappings => {
-      const targetUrl = urlMappings[query];
-      if (targetUrl) {
-        // Add the refer parameter and redirect after a short delay (2 seconds)
-        const referralValue = "https://github.com/SintcoLTD/url";
-        const finalUrl = `${targetUrl}?refer=${referralValue}`;
-        setTimeout(() => {
-          window.location.href = finalUrl;
-        }, 2000); // 2000 milliseconds = 2 seconds
-      } else {
-        console.error("URL not found in url.json");
-      }
-    })
-    .catch(error => {
-      console.error("Error fetching url.json:", error);
-      alert("An error occurred while fetching the URL mappings.");
-    });
+  const urlParams = new URLSearchParams(window.location.search);
+  const targetUrl = urlParams.get("url");
+  if (targetUrl) {
+    // Add the refer parameter and redirect after a short delay (2 seconds)
+    const referralValue = "https://github.com/SintcoLTD/url";
+    const finalUrl = `${targetUrl}?refer=${referralValue}`;
+    setTimeout(() => {
+      window.location.href = finalUrl;
+    }, 2000); // 2000 milliseconds = 2 seconds
+  } else {
+    console.error("URL not found in url parameter");
+  }
 }
 
 // Listen for form submission
